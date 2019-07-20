24314e5636ebc89a42850ad0c2de77675656894a
Alexander Camu <alexander.camu@gmail.com>
2019-07-20 15:58:01 +0200

    Added Data Structure

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,16 @@ package com.company;
 
 public class Main {
 
+    static final int c = 1;
+
     public static void main(String[] args) {
-	// write your code here
-        System.out.printf("Hello World");
+        int x = 3;
+        int y = 4;
+        SimpleBound sB = new SimpleBound(x, y, c);
+        SimpleConstraint sC = new SimpleConstraint();
+        sC.addSimpleBound(sB);
+        CSP P = new CSP();
+        P.addSimpleConstraint(sC);
+        System.out.printf("X = " + P.getSimpleConsraints().get(0).getSimpleBounds().get(0).getX());
     }
 }
